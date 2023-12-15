efe8338fe48bc7fd918b0956767eded643fae3ad
Sanjin Ruzic <rsanjin2004@gmail.com>
2023-12-15 12:32:04 +0100

    Optimized the image loading process and added actions menubar

--- coffeeVendingMachine.m
+++ coffeeVendingMachine.m
@@ -5,9 +5,9 @@ clear all;
 close all;
 clc;
 
-
 % Initialize GUI and get the main frame
 mainFrame = initGUI();
+machineData.buttonsEnabled = 1;
 pause(1);
 
 % Retrieve the data structure associated with the main frame and save it back to the figure
@@ -47,22 +47,31 @@ figureSize = [1000, 700];
 % Calculate the position for the figure to be centered
 mainFramePos =[(screenSize(3) - figureSize(1))/2, (screenSize(4) - figureSize(2))/2, figureSize(1), figureSize(2)];
 
+% Pre-load all coffee images into a cell array
+coffeeImages = cell(1, length(machineData.coffeeTypes));
+for i = 1:length(coffeeImages)
+    coffeeImages{i} = imread(strcat('./Coffee Images/', machineData.coffeeTypes{i}, '.png'));
+end
+
+% Pre-process coffee names and prices into separate arrays
+machineData.coffeeNames = machineData.coffeeTypes;
+machineData.coffeePrices = machineData.coffeePrices;
+
 % Create the GUI frames
 mainFrame = figure('name', 'Coffee Vending Machine Simulation', 'NumberTitle', 'off', 'toolbar', 'none', 'resize', 'off', 'position', mainFramePos, 'visible', 'on');
 mainFrame = gcf;
 titleFrame = uipanel('parent', mainFrame, 'backgroundcolor', titleBackgroundColor, 'position', [0 0.9 1 0.1]);
 coffeeSelectionFrame = uipanel('parent', mainFrame, 'backgroundcolor', backgroundColor, 'position', [0 0 1 0.9]);
 
-% Children uicontrols of titleFrame
+% Text to display the title and the current time
 titleText = uicontrol('parent', titleFrame, 'units', 'normalized', 'style', 'text', 'string', 'COFFEE VENDING MACHINE', 'fontsize', 21, 'fontname', 'Calibri', 'backgroundcolor', titleBackgroundColor, 'foregroundcolor', titleTextColor, 'position', [0.25 0 0.5 1], 'horizontalalignment', 'center', 'fontweight', 'bold');
 clockText = uicontrol('parent', titleFrame, 'units', 'normalized', 'style', 'text', 'string', datestr(now, 'HH:MM'), 'fontsize', 16, 'fontname', 'Calibri', 'backgroundcolor', titleBackgroundColor, 'foregroundcolor', titleTextColor, 'position', [0.8 0 0.25 1], 'horizontalalignment', 'center', 'fontweight', 'bold');
-
 
 spacing = 0.03;
 panelWidth = (1 - 6 * spacing)/5;
 panelHeight = (1 - 5 * spacing)/2;
 
-% This loop creates the coffee panel and calculation the position of each
+% This loop creates the coffee panel and calculates position of each
 for i = 1 : length(machineData.coffeeTypes)
 row = floor((i-1)/5);
 col = mod(i - 1, 5);
@@ -70,6 +79,8 @@ panelX = spacing + col * (panelWidth + spacing);
 panelY = 1 - spacing - (row + 1) * (panelHeight + spacing);
 
 coffeePanel = uipanel('parent', coffeeSelectionFrame, 'backgroundcolor', 'white', 'position', [panelX panelY panelWidth panelHeight], 'bordertype', 'etchedin');
+% On-demand image loading
+uicontrol('parent', coffeePanel, 'style', 'pushbutton', 'units', 'normalized', 'position', [0 0.3 1 0.7], 'callback', {@loadImageCallback, i, coffeeImages, coffeePanel});
 
 % Load images for each type of coffee and display it on the panel
 coffeeImage = imread(strcat('./Coffee Images/', machineData.coffeeTypes{i}, '.png'));
@@ -90,6 +101,11 @@ while ishandle(mainFrame) && ishandle(clockText)
 guidata(mainFrame, machineData);
 end
 
+% Image loading callback function
+function loadImageCallback(hObject, eventdata, i, coffeeImages, panel)
+    set(hObject, 'cdata', coffeeImages{i});
+end
+
 % Callback function for coffee panel click event
 function coffeePanelCallback(hObject, eventdata, coffeeIndex, mainFrame)
 machineData = guidata(mainFrame);
@@ -107,6 +123,22 @@ coffeeTextColor = hex2dec({'00','00','00'})'/255;
 % Create mini window panel that overlays the existing panels
 miniWindow = uipanel('parent', mainFrame, 'backgroundcolor', backgroundColor, 'position', [0.1 0.1 0.8 0.8]);
 
+% Actions menubar
+actions = uimenu('label', 'Actions');
+insertMoneyHeading = uimenu(actions, 'label', '--- Insert Money ---', 'enable', 'off');
+insert1Cent = uimenu(actions, 'label', 'Insert $0.01');
+insert5Cents = uimenu(actions, 'label', 'Insert $0.05');
+insert10Cents = uimenu(actions, 'label', 'Insert $0.10');
+insert25Cents = uimenu(actions, 'label', 'Insert $0.25');
+insert50Cents = uimenu(actions, 'label', 'Insert $0.50');
+insert1Dollar = uimenu(actions, 'label', 'Insert $1.00');
+insert2Dollars = uimenu(actions, 'label', 'Insert $2.00');
+insert5Dollars = uimenu(actions, 'label', 'Insert $5.00');
+takeChangeHeading = uimenu(actions, 'label', '--- Ready to Collect Change? ---', 'enable', 'off');
+collectChange = uimenu(actions, 'label', 'Collect Change');
+takeCoffeeHeading = uimenu(actions, 'label', '--- Ready to Collect Coffee? ---', 'enable', 'off');
+collectCoffee = uimenu(actions, 'label', 'Collect Coffee');
+
 % Display the coffee image on the left side of the panel
 pkg load image;
 selectedCoffeeImage = imread(strcat('./Coffee Images/', machineData.coffeeTypes{coffeeIndex}, '.png'));
@@ -123,9 +155,9 @@ imshow(resizedCoffeeImage, 'parent', coffeeImageAxes);
 coffeeTypeText = uicontrol('parent', miniWindow, 'style', 'text', 'string', machineData.coffeeTypes{coffeeIndex}, 'units', 'normalized', 'position', [0.35 0.9 0.3 0.1], 'backgroundcolor', backgroundColor, 'foregroundcolor', coffeeTextColor, 'fontsize', 20, 'fontname', 'Calibri', 'horizontalalignment', 'left');
 coffeePriceText = uicontrol('parent', miniWindow, 'style', 'text', 'string', ['$ ' sprintf('%.2f', machineData.coffeePrices(coffeeIndex))], 'fontsize', 14, 'fontname', 'Calibri', 'backgroundcolor', backgroundColor, 'foregroundcolor', '#FF5F1F', 'units', 'normalized', 'position', [0.6 0.9 0.1 0.1], 'horizontalalignment', 'right', 'fontweight', 'bold');
 dividerAxes = axes('parent', miniWindow, 'units', 'normalized', 'position', [0.23 0.4 0.85 1], 'visible', 'off');
-dividerLine = line('parent', dividerAxes, 'xdata', [0.35 0.85], 'ydata', [0.85 0.85], 'color', 'k', 'linewidth', 1.5);
+dividerLine = line('parent', dividerAxes, 'xdata', [0.35 0.85], 'ydata', [0.85 0.85], 'color', 'k');
 
-uicontrol('parent', miniWindow, 'style', 'text', 'string', 'Coffee Strength', 'units', 'normalized', 'position', [0.35 0.8 0.3 0.08], 'backgroundcolor', backgroundColor, 'foregroundcolor', 'black', 'fontsize', 14, 'fontname', 'Calibri');
+uicontrol('parent', miniWindow, 'style', 'text', 'string', 'Coffee Strength', 'units', 'normalized', 'position', [0.35 0.8 0.3 0.05], 'backgroundcolor', backgroundColor, 'foregroundcolor', 'black', 'fontsize', 14, 'fontname', 'Calibri');
 for i = 1 : length(machineData.coffeeStrengths)
 uicontrol('parent', miniWindow, 'style', 'togglebutton', 'string', machineData.coffeeStrengths{i}, 'units', 'normalized', 'position', [0.35+(i-1)*0.1 0.7 0.09 0.09], 'backgroundcolor', 'white', 'foregroundcolor', '#FF5F1F', 'fontsize', 10, 'fontname', 'Calibri', 'callback', {@updateButton}, 'fontweight', 'bold');
 end
@@ -135,20 +167,20 @@ for i = 1 : length(machineData.sugarLevels)
     uicontrol('parent', miniWindow, 'style', 'togglebutton', 'string', machineData.sugarLevels{i}, 'units', 'normalized', 'position', [0.35+(i-1)*0.1 0.5 0.09 0.1], 'backgroundcolor', 'white', 'foregroundcolor', '#FF5F1F', 'fontsize', 10, 'fontname', 'Calibri', 'callback', {@updateButton}, 'fontweight', 'bold');
 end
 
+outputMessage = uicontrol('parent', miniWindow, 'style', 'text', 'string', 'placeholder', 'units', 'normalized', 'position', [0.3 0.3 0.3 0.1], 'backgroundcolor', backgroundColor, 'foregroundcolor', coffeeTextColor, 'fontsize', 16, 'fontname', 'Calibri', 'horizontalalignment', 'center');
 guidata(mainFrame, machineData);
 end
 
 % Callback function to update the buttons when one is clicked
-% Callback function to update the buttons when one is clicked
 function updateButton(hObject, ~, buttonIndex)
-    % Get all the buttons
-    allButtons = findobj('style', 'togglebutton');
 
-    % Deselect all buttons and set their color to white with orange text
-    set(allButtons, 'value', 0, 'backgroundcolor', 'white', 'foregroundcolor', '#FF5F1F');
+allButtons = findobj('style', 'togglebutton');
 
-    % Select the clicked button and change its color to orange with white text
-    set(hObject, 'value', 1, 'backgroundcolor', '#FF5F1F', 'foregroundcolor', 'white');
+% Deselect all buttons and set their color to white with orange text
+set(allButtons, 'value', 0, 'backgroundcolor', 'white', 'foregroundcolor', '#FF5F1F');
+
+% Select the clicked button and change its color to orange with white text
+set(hObject, 'value', 1, 'backgroundcolor', '#FF5F1F', 'foregroundcolor', 'white');
 end
 
 
